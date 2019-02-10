702e735446a6aa5d1021430eebf4598f69387592
MirzaDSulistyo <mirzadanu@ymail.com>
2019-02-10 20:04:12 +0700

    Initial Commit

---
+++ Langgan-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Langgan-ios
+//
+//  Created by pramesthi on 10/02/19.
+//  Copyright © 2019 Olsera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
